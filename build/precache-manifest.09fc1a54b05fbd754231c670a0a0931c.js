8a0cedf14918b543e3adea05b67a0ddc3255413c
Achmad Musyaffa Taufiqi <amusfq@gmail.com>
2020-07-10 11:33:30 +0700

    First init

---
+++ build/precache-manifest.09fc1a54b05fbd754231c670a0a0931c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e6488dd32a5e3133e4520514c89372",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfa7d98658729094ab4d",
+    "url": "/static/css/main.89a3f8f1.chunk.css"
+  },
+  {
+    "revision": "8f20a442479cde1039a4",
+    "url": "/static/js/2.66a233c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.66a233c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfa7d98658729094ab4d",
+    "url": "/static/js/main.06064696.chunk.js"
+  },
+  {
+    "revision": "61b9f68396e2d483955d",
+    "url": "/static/js/runtime-main.8cb5001c.js"
+  }
+]);
